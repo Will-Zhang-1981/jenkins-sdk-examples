35dd0d74abaa4ecd904b8b0250b5e306a52f7f9b
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-12 04:01:19 +0100

    List jobs example was added. dummy.txt was deleted.

---
+++ src/main/java/com/github/aistomin/jenkins/examples/jobs/ListJobsExample.java
@@ -0,0 +1,37 @@
+package com.github.aistomin.jenkins.examples.jobs;
+
+import com.github.aistomin.jenkins.Jenkins;
+import com.github.aistomin.jenkins.Job;
+import com.github.aistomin.jenkins.Jobs;
+import com.github.aistomin.jenkins.real.RealJenkins;
+import com.github.aistomin.jenkins.real.UsernamePasswordCredentials;
+import java.util.Iterator;
+
+/**
+ * Created by aistomin on 12/03/16.
+ *
+ * This class demonstrates how to list Jenkins jobs, read details, search for
+ * jobs etc.
+ */
+public class ListJobsExample {
+
+    /**
+     * Start example.
+     * @param args Ignore them.
+     * @throws Exception If something is wrong.
+     */
+    public static void main(String[] args) throws Exception {
+        Jenkins jenkins = new RealJenkins(
+            "https://cisdk-istomin.rhcloud.com",
+            new UsernamePasswordCredentials("integration", "2mFxM5tMVAKHMzTx")
+        );
+        Jobs jobs = jenkins.jobs();
+        System.out.println("----------------- JOBS -----------------");
+        final Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()) {
+            Job job =  iterator.next();
+            System.out.println(job.name());
+        }
+        System.out.println("----------------------------------------");
+    }
+}
