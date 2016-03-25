09a35b1e7ae5b781e769a6d50fccec5fe72dfcb4
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-25 11:34:35 +0100

    - jenkins-sdk version updated to 0.0.2 - ListBuildsExample.java is implemented.

---
+++ src/main/java/com/github/aistomin/jenkins/examples/jobs/ListBuildsExample.java
@@ -0,0 +1,36 @@
+package com.github.aistomin.jenkins.examples.jobs;
+
+import com.github.aistomin.jenkins.Build;
+import com.github.aistomin.jenkins.Jenkins;
+import com.github.aistomin.jenkins.Job;
+import com.github.aistomin.jenkins.real.RealJenkins;
+import com.github.aistomin.jenkins.real.UsernamePasswordCredentials;
+import java.util.Iterator;
+
+/**
+ * Created by aistomin on 25/03/16.
+ *
+ * This class demonstrates how to list Jenkins job builds.
+ */
+public class ListBuildsExample {
+
+    /**
+     * Start example.
+     * @param args Ignore them.
+     * @throws Exception If something is wrong.
+     */
+    public static void main(String[] args) throws Exception {
+        Jenkins jenkins = new RealJenkins(
+            "<YOUR JENKINS URL>",
+            new UsernamePasswordCredentials("<USERNAME>", "<PASSWORD>")
+        );
+        Job job = jenkins.jobs().findByName("<JOB NAME>").next();
+        System.out.println("----------------- BUILDS -----------------");
+        final Iterator<Build> iterator = job.builds().iterator();
+        while (iterator.hasNext()) {
+            Build build =  iterator.next();
+            System.out.println(build.number());
+        }
+        System.out.println("----------------------------------------");
+    }
+}
